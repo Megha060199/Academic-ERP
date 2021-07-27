d4170a7016ab4429574003a2ad8751a2dbad0124
Megha060199 <megha.g.chandwani@gmail.com>
2021-07-27 09:20:43 +0530

    Deleted reports

--- insd_erpnext/acad/report/student_attendance/student_attendance.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2013, INSD and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
