6c0ef952b3f427de9f1de9641ae36f7424eb9e04
Megha060199 <megha.g.chandwani@gmail.com>
2021-07-27 09:27:16 +0530

    changes module errors

--- insd_erpnext/config/desktop.py
+++ insd_erpnext/config/desktop.py
@@ -3,10 +3,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Academics",
+			"module_name": "acad",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Academics")
+			"label": _("acad")
 		}
 	]
