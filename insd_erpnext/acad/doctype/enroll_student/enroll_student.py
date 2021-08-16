eef8cc456dae2a97357afbbdee0d4d6276d262ee
Megha060199 <megha.g.chandwani@gmail.com>
2021-08-16 10:15:54 +0530

    Student field additions and validations

--- insd_erpnext/acad/doctype/enroll_student/enroll_student.py
+++ insd_erpnext/acad/doctype/enroll_student/enroll_student.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2021, INSD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import getdate,today
 from frappe.model.document import Document
-
+import re
 class EnrollStudent(Document):
 	def before_naming(self):
 		self.title = f"{self.first_name} {self.middle_name} {self.last_name}"
+	def validate(self):
+		regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+		if len(self.mobile_number)<10 or len(self.parents_mobile_number)<10 :
+			frappe.throw('Please enter a valid mobile number')
+		if self.date_of_birth and getdate(self.date_of_birth) >= getdate(today()):
+			frappe.throw("Date of Birth cannot be greater than today.")
+		if not (re.fullmatch(regex, self.email_id)):
+			frappe.throw('Please enter a valid email id')
