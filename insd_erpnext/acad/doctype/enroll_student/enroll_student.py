a6b8931ce138dfe18f9b0adff5a1ac36fd2a56a1
Megha060199 <megha.g.chandwani@gmail.com>
2021-09-23 13:07:05 +0530

    Corrected error

--- insd_erpnext/acad/doctype/enroll_student/enroll_student.py
+++ insd_erpnext/acad/doctype/enroll_student/enroll_student.py
@@ -10,8 +10,11 @@ class EnrollStudent(Document):
 		self.title = f"{self.first_name} {self.middle_name} {self.last_name}"
 	def validate(self):
 		regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-		if len(self.mobile_number)<10 or len(self.parents_mobile_number)<10 :
+		if len(self.mobile_number)<10:
 			frappe.throw('Please enter a valid mobile number')
+		if self.parents_mobile_number:
+			if len(self.parents_mobile_number)<10:
+				frappe.throw('Please enter a valid mobile number')
 		if self.date_of_birth and getdate(self.date_of_birth) >= getdate(today()):
 			frappe.throw("Date of Birth cannot be greater than today.")
 		if not (re.fullmatch(regex, self.email_id)):
